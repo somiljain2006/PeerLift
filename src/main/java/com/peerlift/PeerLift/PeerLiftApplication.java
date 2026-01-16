e03c58ab9e31257e450213f349503cf0b96ba891
somiljain2006 <somil16022006@gmail.com>
2026-01-16 19:20:02 +0530

    Initial project commit

---
+++ src/main/java/com/peerlift/PeerLift/PeerLiftApplication.java
@@ -0,0 +1,13 @@
+package com.peerlift.PeerLift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerLiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerLiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peerlift/PeerLift/PeerLiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerlift.PeerLift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerLiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
