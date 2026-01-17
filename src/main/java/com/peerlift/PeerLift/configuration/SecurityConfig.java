a369d9c788d942a048ef6a81f509d48ee605aeec
somiljain2006 <somil16022006@gmail.com>
2026-01-17 14:47:26 +0530

    Deleted extra pushed files

--- src/main/java/com/peerlift/PeerLift/configuration/SecurityConfig.java
+++ src/main/java/com/peerlift/PeerLift/configuration/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("*"));
+		config.setAllowedOriginPatterns(List.of("*"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setExposedHeaders(List.of("Authorization"));
